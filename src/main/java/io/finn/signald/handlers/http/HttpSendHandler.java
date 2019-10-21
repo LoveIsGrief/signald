da351fe754ba5faedee537e6868a14403dd92b26
LoveIsGrief <loveisgrief@tuta.io>
2019-10-21 12:15:28 +0200

    Beginning of HttpSendHandler

--- src/main/java/io/finn/signald/handlers/http/HttpSendHandler.java
+++ src/main/java/io/finn/signald/handlers/http/HttpSendHandler.java
@@ -1,5 +1,6 @@
 package io.finn.signald.handlers.http;
 
+import io.finn.signald.JsonQuote;
 import io.finn.signald.JsonRequest;
 import io.finn.signald.handlers.BaseJsonHandler;
 import spark.Request;
@@ -11,6 +12,15 @@ public class HttpSendHandler extends BaseHttpHandler {
 
     @Override
     protected JsonRequest convertRequest(Request request) {
-        return null;
+        JsonRequest jsonRequest = new JsonRequest();
+        if(request.queryMap("quote").hasKeys()){
+            JsonQuote quote = new JsonQuote();
+//            quote.id =
+//            jsonRequest.quote = quote;
+        }
+        jsonRequest.username = request.queryMap("username").value().trim();
+        jsonRequest.recipientNumber =  request.queryMap("recipientNumber").value().trim();
+
+        return jsonRequest;
     }
 }
